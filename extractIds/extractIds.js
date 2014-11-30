45639dfdbc9a71f90ff0a08a3f1a17299f6303f9
jhenriquez <julio.m.henriquez@gmail.com>
2014-11-30 00:52:40 -0400

    Added Kata description.

--- extractIds/extractIds.js
+++ extractIds/extractIds.js
@@ -1,3 +1,25 @@
+/*
+Complete the method so that it returns an array of all ID's passed in.
+The data structure will be similar to the following:
+*/
+
+var data = {
+  id: 1,
+  items: [
+    {id: 2},
+    {id: 3, items: [
+      {id: 4},
+      {id: 5}
+    ]}
+  ]
+};
+
+/*
+The method should be able to handle the case of empty data being passed in.
+
+Note: The only arrays that need to be traversed are those assigned to the "items" property.
+*/
+
 function extractIds (data) {
 	var ids = data && data.id ? [data.id] : [];
 	var items = data && data.items && Array.isArray(data.items) ? data.items : [];
@@ -6,5 +28,7 @@ function extractIds (data) {
 
 	return ids;
 }
+
+extractIds(data); // should return [1,2,3,4,5]
 
 module.exports = extractIds;
