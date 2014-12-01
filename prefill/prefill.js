8a4ab94b41fa4621e9d75e6f4f71a2ddfc65617b
jhenriquez <julio.m.henriquez@gmail.com>
2014-12-01 17:08:27 -0400

    Added prefill solution.

---
+++ prefill/prefill.js
@@ -0,0 +1,31 @@
+/*
+Create the function prefill that returns an array of n elements that all have the same value v. 
+See if you can do this without using a loop.
+
+You have to validate input:
+
+v can be anything (primitive or otherwise)
+if v is ommited, fill the array with undefined
+if n is 0, return an empty array
+if n is anything other than an integer or integer-formatted string (e.g. '123') that is >=0, throw a TypeError
+When throwing a TypeError, the message should be n is invalid, where you replace n for the actual value passed to the function.
+
+*/
+
+function isInt(value) {
+  return !isNaN(value) && 
+         parseInt(Number(value)) == value && 
+         !isNaN(parseInt(value, 10));
+}
+
+function prefill (n, v) {
+	if (isNaN(n) || (+n) < 0 || n != parseInt(n,10)) {
+		throw TypeError(n + ' is invalid');
+	}
+
+	if (+n === 0) return [];
+
+	return [typeof(v) === 'function' ? v() : v].concat(prefill((+n)-1,v));
+}
+
+module.exports = prefill;
