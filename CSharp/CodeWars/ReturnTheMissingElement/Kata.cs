065bdef228511782a3951342339f2ec116d787a6
Julio <julio.m.henriquez@gmail.com>
2015-09-14 08:00:22 -0400

    Solved Return The Missing Element

---
+++ CSharp/CodeWars/ReturnTheMissingElement/Kata.cs
@@ -0,0 +1,22 @@
+﻿namespace CodeWars.ReturnTheMissingElement
+{
+    class Kata
+    {
+        public int GetMissingElement(int[] superImportantArray)
+        {
+            int missing = -1;
+
+            string compare_elements = string.Join("", superImportantArray);
+
+            for (var i = 0; i < 10; i++)
+            {
+                if (compare_elements.IndexOf(i.ToString()[0]) == -1)
+                {
+                    missing = i;
+                }
+            }
+
+            return missing;
+        }
+    }
+}
---
+++ CSharp/CodeWars/ReturnTheMissingElement/ReturnTheMissingElementSpecs.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeWars.ReturnTheMissingElement
+{
+    [TestClass]
+    public class ReturnTheMissingElementSpecs
+    {
+        ReturnTheMissingElement.Kata _kata = new ReturnTheMissingElement.Kata();
+
+        [TestMethod]
+        public void It_Should_return_8_when_provided_with_051329764()
+        {
+            var expected = 8;
+            var actual   = _kata.GetMissingElement( new int[] { 0, 5, 1, 3, 2, 9, 7, 6, 4 });
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void It_Should_return_3_when_provided_with_924570861()
+        {
+            var expected = 3;
+            var actual = _kata.GetMissingElement( new int[] { 9, 2, 4, 5, 7, 0, 8, 6, 1 });
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
