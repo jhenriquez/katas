7abedd6b274fb69fb8fcc1d0f3f4502147351a13
Julio <julio.m.henriquez@gmail.com>
2015-09-14 18:16:16 -0400

    Solved Sum Of First Nth Series

---
+++ CSharp/CodeWars/SumOfFirstNthTermOfSeries/NthSeries.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CodeWars.SumOfFirstNthTermOfSeries
+{
+    public class NthSeries
+    {
+
+        public string SeriesSum(int num)
+        {
+            double result = 0.00;
+            double div = 1.00;
+
+            for(var i = 0; i < num;i++)
+            {
+                result += 1 / div;
+                div += 3;
+            }
+
+            return result.ToString("0.00");
+        }
+    }
+}
---
+++ CSharp/CodeWars/SumOfFirstNthTermOfSeries/SumOfFirstNthTermOfSeriesSpecs.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeWars.SumOfFirstNthTermOfSeries
+{
+    [TestClass]
+    public class NthSeriesSpecs
+    {
+        SumOfFirstNthTermOfSeries.NthSeries _kata = new SumOfFirstNthTermOfSeries.NthSeries();
+
+        [TestMethod]
+        public void Should_Return_Zero_For_SumOfZero()
+        {
+            var expected = "0.00";
+            var actual = _kata.SeriesSum(0);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Should_Return_1dot25_For_SumOfTwo()
+        {
+            var expected = "1.25";
+            var actual = _kata.SeriesSum(2);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
