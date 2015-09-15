6b42b76d728bfab07fc5b754e83ade66dea5519c
Julio <julio.m.henriquez@gmail.com>
2015-09-14 20:14:54 -0400

    Solved The Old Switcheroo 2

---
+++ CSharp/CodeWars/TheOldSwitcherooTwo/OldSwitcherooTwo.cs
@@ -0,0 +1,29 @@
+﻿namespace CodeWars.TheOldSwitcherooTwo
+{
+    public class OldSwitcherooTwo
+    {
+        public string Encode(string str)
+        {
+            var alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            for(var i = str.Length - 1; i >= 0; i--)
+            {
+                var indexOf = alphabet.IndexOf(str[i]);
+
+                if (indexOf == -1)
+                {
+                    continue;
+                }
+
+                if (indexOf > 25)
+                {
+                    indexOf -= 26;
+                }
+
+                str = str.Remove(i, 1).Insert(i, (indexOf + 1).ToString());
+            }
+
+            return str;
+        }
+    }
+}
---
+++ CSharp/CodeWars/TheOldSwitcherooTwo/OldSwitcherooTwoSpecs.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeWars.TheOldSwitcherooTwo
+{
+    [TestClass]
+    public class OldSwitcherooTwoSpecs
+    {
+        TheOldSwitcherooTwo.OldSwitcherooTwo _kata = new TheOldSwitcherooTwo.OldSwitcherooTwo();
+
+        [TestMethod]
+        public void It_Should_Replace_Vowls_With_Index_Within_A_String()
+        {
+            var expected = "315452311819";
+            var actual = _kata.Encode("codewars");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void It_Should_Not_Be_Affected_By_The_Modified_String()
+        {
+            var expected = "208919 919 1 1215147 1920189147!! 161251195 [51431545] @30181853201225";
+            var actual = _kata.Encode("this is a long string!! Please [encode] @C0RrEctly");
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
