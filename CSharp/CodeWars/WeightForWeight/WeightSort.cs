9cfac212e46b41b43fddd87d44082e58954a1cac
Julio <julio.m.henriquez@gmail.com>
2015-09-15 20:54:41 -0400

    Solved Weight For Weight

---
+++ CSharp/CodeWars/WeightForWeight/WeightSort.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace CodeWars.WeightForWeight
+{
+    public class WeightSort
+    {
+        public string OrderWeight(string strng)
+        {
+            return strng.Split(' ')
+                .Select(w => new { Weight = w, Value = GetWeightValue(w) })
+                .OrderBy(w => w.Value)
+                .ThenBy(w => w.Weight)
+                .Select(w => w.Weight)             
+                .Aggregate((r, v) => r + ' ' + v)
+                .Trim();
+        }
+
+        private int GetWeightValue(string weight)
+        {  
+            return weight.ToCharArray().Select(Char.GetNumericValue).Select(Convert.ToInt32).Sum();
+        }
+    }
+}
---
+++ CSharp/CodeWars/WeightForWeight/WeightSortSpecs.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeWars.WeightForWeight
+{
+    [TestClass]
+    public class WeightSortSpecs
+    {
+        WeightForWeight.WeightSort _kata = new WeightForWeight.WeightSort();
+
+        [TestMethod]
+        public void OrderWeight_Should_Return_100_20_When_Passed_20_100()
+        {
+            var expected = "100 20";
+            var actual = _kata.OrderWeight("20 100");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void OrderWeight_Should_Order_Equaly_Valued_Weights_In_Descending_Order()
+        {
+            var expected = "11 11 2000 10003 22 123 1234000 44444444 9999";
+            var actual = _kata.OrderWeight("44444444 2000 11 11 10003 22 123 1234000  9999");
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
